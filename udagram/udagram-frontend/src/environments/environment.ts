97fd283e1584c345a62f0788cfc21acf4baa28d7
Mahmoud hussein <engmahmoudhussein2022@gmail.com>
2022-07-04 20:23:45 +0200

    final project files 2

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev1.eba-m3sptrmy.us-east-1.elasticbeanstalk.com//api/v0'
+  apiHost: 'http://udagram-api-dev1.eba-m3sptrmy.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
